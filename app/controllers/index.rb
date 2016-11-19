6aadec1fce358abf838b9cc7bc90d144a4807e11
codeamac19 <codeamac19@gmail.com>
2016-11-18 18:50:28 -0600

    Programa que toma los datos de una forma usando ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,23 +9,30 @@ end
 #ESTE ES UN METODO POST
 post '/abuelita' do
       #ESTE METODO ES SMILIAR A UN GETS CHOMP 
-      user_input = params[:user_input]
-  
-     case 
-      when user_input == user_input.upcase && user_input != "BYE TQM"
-        @abuelita = "NO, NO DESDE 1983"
-      when user_input == user_input.downcase 
-        @abuelita =  "HUH?! NO TE ESCUCHO, HIJO!"
-      when user_input == "BYE TQM"
-        @abuelita = "BYE MIJITO!"
-        redirect to ("/?abuelita=#{@abuelita}")
-      else
-        @abuelita = "HUH?! NO TE ESCUCHO, HIJO!" 
+    user_input = params[:user_input]
+    response = grandma(user_input)   
+    if request.xhr?
+       # respond to Ajax request
+       # grandma(user_input)
+       #response
+    else
+       # respond to normal request
+       redirect to ("/?abuelita=#{response}")
     end
-  #ESTE ES EL COMANDO PARA REFIRECCIONAR
-  #("/?abuelita=#{@abuelita}") ESTE ES UN QUERRY STRING
-  # LA DIAGONAL ES RAIZ 
-  redirect to ("/?abuelita=#{@abuelita}")
+ end
+
+
+def grandma(user_input)
+  case 
+      when user_input == user_input.upcase && user_input != "BYE TQM"
+        "NO, NO DESDE 1983"
+      when user_input == user_input.downcase 
+        "HUH?! NO TE ESCUCHO, HIJO!"
+      when user_input == "BYE TQM"
+        "BYE MIJITO!"
+      else
+        "HUH?! NO TE ESCUCHO, HIJO!" 
+  end
 end
 
 
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
 	// de HTML este exista ya en la página. 
 
+
+  // $( "#target" ).submit(function( event ) {
+      
+  //     event.preventDefault();
+  //     userInput = $(this).serialize(); //serialize // vas a obtener los valores de entrada en la forma
+  //     console.log(userInput);
+  //     $.post("/abuelita", userInput, function(data) {
+  //      $(".abue").html(data);
+
+  //      });
+  // });
+
+
+
 });
